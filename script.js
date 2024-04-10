7066f36f8eae86a743537a2883617aa6d236d4ba
alangt22 <145603980+alangt22@users.noreply.github.com>
2024-04-09 22:00:10 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let show = true;
+const menuContent = document.querySelector('.content'); 
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+
+menuToggle.addEventListener('click', () => {
+
+    menuContent.classList.toggle('on', show);
+    show = !show;
+
+});
